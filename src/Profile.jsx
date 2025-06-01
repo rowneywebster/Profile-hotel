decad2369ce52c3d59b98e539af5a190549c0558
Rowney Ouma <oumarowney@gmail.com>
2025-06-01 19:12:41 +0300

    updated

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './App.css';
+import './Profile.css';
 
 const Profile = () => {
   const navigate = useNavigate();
